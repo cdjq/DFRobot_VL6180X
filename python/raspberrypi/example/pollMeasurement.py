6c71df8571928e63c3a87d0cef21c27b66099725
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-04-28 08:48:11 +0800

    Update pollMeasurement.py

--- python/raspberrypi/example/pollMeasurement.py
+++ python/raspberrypi/example/pollMeasurement.py
@@ -15,24 +15,25 @@ sys.path.append('../')
 from DFRobot_VL6180X import DFRobot_VL6180X
 import time
 import RPi.GPIO as GPIO
-#当iic地址被修改后，应当在实例化类时传入更改后的iic地址。iic地址被更改后掉电保存，但是，如果使用了CE引脚进行了传感器重启，iic地址会变回默认地址0x29
+#When the IIC address is changed, the address should be passed in when the class is instantiated. And it will be saved after its changing. 
+#But if the sensor is restarted by using the CE pin, the IIC address will change back to the default address 0x29
 # bus ：iic bus
 VL6180X = DFRobot_VL6180X(iic_addr = 0x39,bus = 1)
 
 
 while(VL6180X.begin() == False ):
   print ('Please check that the IIC device is properly connected')
-# 更改IIC地址
+# Change IIC address
 VL6180X.set_iic_addr(0x29)
 
 while True:
-  # 获取环境光数据
+  # Get ambient light data
   lux = VL6180X.als_poll_measurement()
   print('ALS vlaue : %f lux'%lux)
   time.sleep(1)
-  # 获取距离数据
+  # Get range data
   range = VL6180X.range_poll_measurement()
-  # 获取距离数据的判断结果
+  # Get the judgment of the range data
   status = VL6180X.get_range_result()
   
   if(status ==VL6180X.VL6180X_NO_ERR ):
